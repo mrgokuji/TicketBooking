847e26b29ad1857baa9242c1c73b49e5c35fe6c4
mrgokuji <harsh>
2021-11-28 11:19:49 +0530

    added services for APIs using JPA repo interface

---
+++ ticket/src/main/java/com/booking/ticket/seat/SeatService.java
@@ -0,0 +1,36 @@
+// this file will contain the implementaion for the method for APIs.
+package com.example.bookingsystem.seat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SeatService {
+
+    private final SeatRepository seatRepository;
+
+    @Autowired
+    public SeatService(SeatRepository seatRepository) {
+        this.seatRepository = seatRepository;
+    }
+
+    public List<Seat> getSeats() {
+        return seatRepository.findAll();
+    }
+
+    public List<Integer> bookSeat(List<Integer> ids) {
+        List<Seat> seats = seatRepository.findAllById(ids);
+        List<Integer> seat_not_booked = new ArrayList();
+        // save the id to DB. and return the success code 0; if some conflicting seat then return the conflicting values.
+        for (Seat seat : seats) {
+            if (!seat.getBooked()) {
+                seat.setBooked(true);
+            }
+            else seat_not_booked.add(seat.getId)
+        }
+        seatRepository.saveAll(seats);
+        return seat_not_booked;
+    }
+
+}
