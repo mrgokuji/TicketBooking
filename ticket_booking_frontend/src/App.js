8af22c92402122352fe24dc4982589c978ef30b6
mrgokuji <harsh>
2021-11-28 10:37:38 +0530

    modified some dependies

--- ticket_booking_frontend/src/App.js
+++ ticket_booking_frontend/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ // eslint-disable-next-line 
+ /* eslint-disable */
+ import './App.css'
+ import React, { useState } from 'react'
+ import { Cinema } from './components/cinema';
+ 
+ const movies = [
+   {
+     name: 'Avenger',
+     price: 10,
+     occupied: [20, 21, 30, 1, 2, 8],
+   },
+   {
+     name: 'Joker',
+     price: 12,
+     occupied: [9, 41, 35, 11, 65, 26],
+   },
+   {
+     name: 'Toy story',
+     price: 8,
+     occupied: [37, 25, 44, 13, 2, 3],
+   },
+   {
+     name: 'the lion king',
+     price: 9,
+     occupied: [10, 12, 50, 33, 28, 47],
+   },
+ ]
+ 
+ 
+ export default function App() {
+ 
+   return (
+     <div className="App">
+       <ShowCase />
+       <Cinema />
+ 
+       <p className="info">
+         You have selected <span className="count">{10}</span>{' '}
+         seats for the price of{' '}
+       </p>
+     </div>
+   )
+ }
+ 
+ function ShowCase() {
+   return (
+     <ul className="ShowCase">
+       <li>
+         <span className="seat" /> <small>N/A</small>
+       </li>
+       <li>
+         <span className="seat selected" /> <small>Selected</small>
+       </li>
+       <li>
+         <span className="seat occupied" /> <small>Occupied</small>
+       </li>
+     </ul>
+   )
+ }
+ 
+ 
+ 
