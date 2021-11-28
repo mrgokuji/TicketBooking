96ae5f36b92dbbb68123a6ac769111fe9cbe3695
mrgokuji <harsh>
2021-11-28 12:06:39 +0530

    big fix 1

--- ticket/src/main/java/com/booking/ticket/seat/SeatController.java
+++ ticket/src/main/java/com/booking/ticket/seat/SeatController.java
@@ -11,26 +11,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class SeatController
 {
+
+    private final SeatService seatService;
+
     @RequestMapping("getSeats")
     public List<Seat> getSeat(){
-        List<Seat> seats = new ArrayList();
-        Seat s1 = new Seat();
-        s1.setId(1);
-        s1.setCost(100);
-        s1.setBooked(false);
-        Seat s2 = new Seat(2,false,100);
-        seats.add(s1);
-        seats.add(s2);
-        return seats;
+        return seatService.getSeats();
     }
 
     @RequestMapping("bookSeats")
     public List<Integer> bookSeats(@RequestBody List<Integer> ids){
-        System.out.println(ids);
-        List<Integer> seat_not_booked = new ArrayList();
-        // save the id to DB. and return the success code 0; if some conflicting seat then return the 
-        seat_not_booked.add(2);
-        return seat_not_booked;
+        return seatService.bookSeat(ids);
     }
     @RequestMapping("")
     public Integer root(){
